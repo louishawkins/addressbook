928b80edd03644a631972dcdda9047070fc5928e
Louis Hawkins <louishawkins@gmail.com>
2014-11-24 13:37:38 -0600

    basic files needed

---
+++ public/index.php
@@ -0,0 +1,41 @@
+
+<?php // include stuff ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Address Book</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<!-- header -->
+
+	<div class="row" id="header">
+		<div class="container">
+			<h1>Address Book Application</h1>
+			<p><a href="#addperson">Add Person</a></p>
+		</div> <!-- header row container -->
+	</div> <!-- header row div -->
+
+<!-- main row here -->
+
+	<div class="row" id="mainrow">
+		<div class="container">
+			<h2>The address cards will be displayed in here</h2>
+		</div>
+	</div>
+
+<!-- footer row here -->
+
+	<div class="row " id="footer">
+		<div class="container">
+			<h3>Footer content goes here</h3>
+		</div>
+	</div>
+
+</div> <!-- body container -->
+</body>
+</html>
