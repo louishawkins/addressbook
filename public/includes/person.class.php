88e4f7e70ec9b73031ee9b6fb4e96d0e84d17e24
pkuzma101 <kuzma.paul@gmail.com>
2014-11-26 11:12:26 -0600

    First draft of person class

--- public/includes/person.class.php
+++ public/includes/person.class.php
@@ -1,48 +1,63 @@
 <?php
 
-define ('DB_HOST', '127.0.0.1');
-define ('DB_NAME', 'addressbook');
-define ('DB_USER', 'pkuzma101');
-define ('DB_PASS', 'duster2');
+include 'includes/config.inc.php';
 
-require "dbconnect.php";
-
-class Person {
-
-public $new_post = array();
-public $dbc;
-
-function __construct($post, $dbc) {
-	$this->new_post = $post;
-	$this->dbc = $dbc;
+Class Person {
+	
+public function __construct($post, $dbc) {
+	$this->$dbc;
+	$this->sanitize($post);
 }
 
-$stmt = $dbc->prepare("SELECT first_name, last_name, p_id, ph_num, image FROM people");
-$stmt->execute();
+protected $first_name; = $new_entry['first_name'];
+protected $last_name; = $new_entry['last_name'];
+protected $ph_num; = $new_entry['ph_num'];
+protected $image; = $new_entry['image'];
 
-public $first_name = $stmt->fetchColumn(1);
+public $passedValidation = false;
 
-public $last_name = $stmt->fetchColumn(2);
+private function sanitize($post) {
+	foreach($post as $key => $value) {
+		$post[$key] = htmlspecialchars(strip_tags($value));
+	}
+	$this->validatePerson($post);
+}
 
-public $ph_num = $stmt->fetchColumn(3);
+private function validatePerson($post) {
+	if(isset($post['first_name']) && isset($post['last_name']) && isset($post['ph_num'])) {
+		$this->passedValidation = true;
+	}
+}
 
-public $image = $stmt->fetchColumn(4);
+public function read_contact_with_addresses() {
+	$query = "SELECT p.p_id, p.first_name, p.last_name, p.ph_num, p.image, a.add_id, a.street, a.city, a.state, a.zip 
+				FROM people AS p
+				JOIN address_person AS pivot ON p.p_id = pivot.person_id
+				JOIN addresses AS a ON a.add_id = pivot.address_id";
 
-function split_name($input) {
+	$stmt = $dbc->prepare($query);
+	$stmt->execute();
+
+}
+
+public function insert_new_person() {
+	$query = $dbc->prepare("INSERT INTO people(first_name, last_name, ph_num, image)
+							VALUES(:first_name, :last_name, :ph_num, :image)");
+	$query->bindValue(":first_name", $new_entry['first_name'], PDO::PARAM_STR);
+	$query->bindValue(":last_name", $new_entry['last_name'], PDO::PARAM_STR);
+	$query->bindValue(":ph_num", $new_entry['ph_num'], PDO::PARAM_STR);
+	$query->bindValue(":image", $new_entry['image'], PDO::PARAM_STR);
+	$query->execute();
+	$p_id = $dbc->lastInsertId();
+}
+
+public function split_name($input) {
+		$result = array();
 		$parts = explode(" ", $input, 2);
-		$last_name = array_pop($parts);
-		$first_name = implode(" ", $parts);
-		return $first_name . " " . $last_name;
+		$result['last_name'] = array_pop($parts);
+		$result['first_name'] = implode(" ", $parts);
+		return $result;
 }
 
 
-}
-
-$tor = new Person;
-$this->first_name;
-
-if(isset($_POST)){
-	$new_person = new Person($_POST, $dbc);
-}
 ?>
-
